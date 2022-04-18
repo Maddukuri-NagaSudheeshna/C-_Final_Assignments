792b5a0268e620a893f240b7eea7eee6a3181ecb
Maddukuri-NagaSudheeshna <102571111+Maddukuri-NagaSudheeshna@users.noreply.github.com>
2022-04-18 13:28:38 +0530

    Add files via upload

---
+++ LitwareLib.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMSForLitwareOrganization
+{
+    internal class LitwareLib : IPrintable
+    {
+
+        public void LoginForLitwareOrg() 
+        {
+            Console.WriteLine("Please Login Using EmployeeId and EmployeeName");
+        }
+        public void LogoutfromLitwareOrg() 
+        {
+            Console.WriteLine("your LoginSession Expired");
+        }
+
+        static void Main(string[] args)
+        {
+            LitwareLib lit = new LitwareLib();
+            lit.LoginForLitwareOrg();
+            int TotalEmployeeCount = 10;
+            for(int i=1;i<=TotalEmployeeCount;i++)
+            {
+                Employee emp = new Employee();
+                emp.Method1(101);
+                emp.Method2("kishan kumar");
+                emp.Method3();
+            }
+            lit.LogoutfromLitwareOrg();
+
+            /*emp.Method1(101);
+            emp.Method2("Sudheeshna");
+            emp.Method3(); */   
+
+
+            //emp.MethodforHRA();
+            //emp.MethodForGrossSalary();
+            //emp.CalculateSalary();
+        }
+       
+    }
+}
---
+++ Manager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMSForLitwareOrganization
+{
+    internal abstract class Manager : Employee 
+    {
+        public abstract string Name { get; } 
+        Employee employee = new Employee();
+
+        private double PetrolAllowance;
+        private double FoodAllowance;
+        private double OtherAllowances;
+        
+        private void CalculatePetrolAllownace(double Salary)
+        {
+            this.Salary = Salary;
+            PetrolAllowance = (8 / Salary) * 100;
+            Console.WriteLine("Petrol allowances {0}", PetrolAllowance);
+        }
+        private void CalculateFoodAllowance(double Salary)
+        {
+            this.Salary = Salary;
+            FoodAllowance = (13/ Salary) * 100;
+            Console.WriteLine("food allowance {0}",FoodAllowance);
+        }
+        private void CalculateOtherAllowances(double Salary)
+        {
+            this.Salary = Salary;
+            OtherAllowances = (3 / Salary) * 100;
+            Console.WriteLine("Other Allowances {0}",OtherAllowances);
+
+        }
+        /*public double HRA { get; }                
+        public override void MethodForGrossSalary()
+        {
+            Employee employee = new Employee();
+            
+        }*/
+        public override void CalculateSalary()
+        {
+            Employee emp = new Employee();
+            Console.WriteLine("Overridden CalculateSalary method from Employee Class");
+            emp.CalculateSalary();
+        }
+
+
+
+
+    }
+}
